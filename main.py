f10c6a2648604f51f2af1c3e5cdb8665ba071e65
Alper Berk <alperberk.k@hotmail.com>
2023-08-16 18:11:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+for fzb in range(1, 101):
+    if fzb % 3 == 0:
+        print("Fizz")
+    elif fzb % 5 ==0:
+        print("Buzz")
+    elif fzb % 15 ==0:
+        print("FizzBuzz")
+    else:
+        print(fzb)
+print(fzb)
